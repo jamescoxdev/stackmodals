dc3baa6c83de456d1f5280f802531b3819794704
James Cox <60166857+jamescoxdev@users.noreply.github.com>
2020-02-04 16:24:17 +1100

    Update confirm.js

--- src/components/modals/confirm.js
+++ src/components/modals/confirm.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Confirm(props){
     return(
         <div className={props.className} style={props.show}>
-            {props.text}
+            <div style={{padding:'20px'}}>
+                {props.text}
+            </div>
             <section className="btns">
                 <button onClick={props.closeConfirm}>Cancel</button>
                 <button onClick={() => { props.confirmFunction(); props.closeConfirm() }}>{props.confirmBtnName}</button>
