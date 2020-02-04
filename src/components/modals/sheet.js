49c79f18164529ce577abd200064111e8cfaa8cf
James Cox <60166857+jamescoxdev@users.noreply.github.com>
2020-02-04 16:24:58 +1100

    Update sheet.js

--- src/components/modals/sheet.js
+++ src/components/modals/sheet.js
@@ -3,7 +3,9 @@ import React from 'react';
 export default function Sheet(props){
     return(
         <div className={props.className} style={props.show}>
-            {props.text}
+            <div style={{padding:'20px'}}>
+                {props.text}
+            </div>
             <section className="btns">
                 <button onClick={props.closeSheet}>Ok</button>
                 <div className="clear"></div>
